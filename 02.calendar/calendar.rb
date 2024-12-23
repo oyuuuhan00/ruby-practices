8f740463e85dad78d94f7c383ac0746383b12848
oyuuuhan <oyuuuhan@gmail.com>
2024-12-24 07:35:27 +0900

    2回目のレビューポイントを修正しました

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -1,44 +1,32 @@
 #!/usr/bin/env ruby
-require "debug"
 require 'date'
 require "optparse"
 
 option = {}
 OptionParser.new do |opt|
-  opt.on('-m value') {|n| option[:m] = n}
-  opt.on('-y value') {|n| option[:y] = n}
+  opt.on('-m value') { |n| option[:m] = n }
+  opt.on('-y value') { |n| option[:y] = n }
   opt.parse!(ARGV)
 end
 
-if option[:m]
-  month = option[:m].to_i
-else
-  month = Date.today.month 
-end
+month = option[:m]&.to_i || Date.today.month
 
-if option[:y]
-  year = option[:y].to_i
-else
-  year = Date.today.year
-end
+year = option[:y]&.to_i || Date.today.year
 
 cell_width = 2
 
-firstday_date = Date.new(year, month, 1).day
-lastday_date = Date.new(year, month, -1).day
-weekday = ["日", " 月", " 火", " 水", " 木", " 金", " 土"]
-start_day_of_week = Date.new(year, month, 1).wday
+firstday_date = Date.new(year, month, 1)
+lastday_date = Date.new(year, month, -1)
+weekday = "日 月 火 水 木 金 土"
+start_day_of_week = firstday_date.wday
 
 head = "#{month}月 #{year}"
 puts head.center(20)
-weekday.each do |week|
-  print week
-end
+print weekday
 puts "\n"
 
-print " " * (cell_width * start_day_of_week + start_day_of_week)
-(firstday_date..lastday_date).each do |day|
-  date = Date.new(year, month, day)
+print "   " * start_day_of_week
+(firstday_date..lastday_date).each do |date|
   print date.day.to_s.rjust(2) + " "
   print "\n" if date.saturday?
 end
