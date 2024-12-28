7f86dbf1aaa11127110895d7ae04a9266e621a8e
oyuuuhan <oyuuuhan@gmail.com>
2024-12-28 23:17:03 +0900

    2回目のレビューを修正しました

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -16,19 +16,21 @@ end
 frames = shots.each_slice(2).to_a
 
 point = frames.each_with_index.sum do |frame, index|
-  frame_sum = frame.sum
+  base_sum = frame.sum
 
-  if index < 9
-    if frame[0] == 10 # Strike
-      frame_sum +
-        frames[index + 1][0] +
-        (frames[index + 1][0] == 10 ? frames[index + 2][0] : frames[index + 1][1])
-    else
-      frame_sum == 10 ? frame_sum + frames[index + 1][0] : frame_sum # Spareまたはそれ以外
-    end
-  else
-    frame_sum
-  end
+  add_sum = if index < 9
+              if frame[0] == 10 # Strike
+                frames[index + 1][0] +
+                  (frames[index + 1][0] == 10 ? frames[index + 2][0] : frames[index + 1][1])
+              elsif base_sum == 10 # Spare
+                frames[index + 1][0]
+              else
+                0
+              end
+            else
+              0
+            end
+  base_sum + add_sum
 end
 
 puts point
